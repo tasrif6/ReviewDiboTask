9d7ecfe9f47d23fe7edfaf37bf321a5d641b787a
Md Tasrif Khan <tasrif.k.56@gmail.com>
2026-06-30 20:13:31 +0600

    cors changed

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "https://review-dibo-task.vercel.app/",
+       "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
